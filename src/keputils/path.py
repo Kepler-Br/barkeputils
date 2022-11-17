a2d9d25b6ea769069a55ed4244d346358d5e67e4
Barrett Kepler <45532267+Kepler-Br@users.noreply.github.com>
2022-11-17 09:03:46 +0300

    dunno

--- src/keputils/path.py
+++ src/keputils/path.py
@@ -4,8 +4,8 @@ from typing import Callable, Iterable
 
 def walk_files(
         start_folder: str,
-        file_filter: Callable[[Iterable[str]], bool] = None,
-        folder_filter: Callable[[Iterable[str]], bool] = None
+        file_filter: Callable[[str], bool] = None,
+        folder_filter: Callable[[str], bool] = None
 ) -> tuple[str, tuple[str], tuple[str]]:
     for root, folders, files in os.walk(start_folder, topdown=True):
         if file_filter is None:
